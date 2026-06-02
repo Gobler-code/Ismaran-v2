4149d1be1d8bddcf69878b66c3581b469883e698
Gobler-code <goblergoss@gmail.com>
2026-06-02 09:11:28 +0545

    save src file

---
+++ backend/src/models/Document.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const documentSchema = new mongoose.Schema(
+    {
+        title:{
+            type: String,
+            required:[true, 'Title is required'],
+        },
+        content:{
+            type: String,
+            required : [true,'Content is required'],
+        },
+       user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+       }
+    },
+    {timestamps:true}
+);
+
+module.exports = mongoose.model('Document', documentSchema);
---
+++ backend/src/models/Quiz.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const quizSchema = new mongoose.Schema(
+    {
+        question:{
+            type: String,
+            required:[true, 'Title is required'],
+        },
+        option:{
+            type: [String],
+            required : [true,'Option is required'],
+        },
+        correctAnswer:{
+               type: String,
+               require: [true,'Option is required'],
+               
+        },
+        document:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Document',
+            required:true,
+        },
+       user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+       }
+    },
+    {timestamps:true}
+);
+
+module.exports = mongoose.model('Quiz', quizSchema);

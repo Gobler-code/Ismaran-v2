d7912a8f132d90f9eabf84be6c7a29688fcdb212
Gobler-code <goblergoss@gmail.com>
2026-06-07 13:39:19 +0545

    create protected routes for ai response

--- backend/src/routes/aiRoutes.js
+++ backend/src/routes/aiRoutes.js
@@ -5,11 +5,21 @@ const protect  = require('../middleware/authMiddleware');
 const { generateFlashcards, 
     generateQuiz, 
     generateHighlight, 
-    generateVocab } = require('../controllers/aiController');
+    generateVocab,
+    getFlashcards,
+    getQuiz,
+    getHighlight,
+    getVocab } = require('../controllers/aiController');
 
 router.post('/flashcards/:documentId',protect,generateFlashcards);
 router.post('/quiz/:documentId',protect,generateQuiz);
 router.post('/highlights/:documentId',protect,generateHighlight);
 router.post('/vocab/:documentId',protect,generateVocab);
 
+
+
+router.get('/flashcards/:documentId', protect, getFlashcards);
+router.get('/quiz/:documentId', protect, getQuiz);
+router.get('/highlights/:documentId', protect, getHighlight);
+router.get('/vocab/:documentId', protect, getVocab);
 module.exports = router;

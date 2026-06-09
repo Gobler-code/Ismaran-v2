a47f1f4295fbe0d4eac6c5ca089a916b8a96b476
Gobler-code <goblergoss@gmail.com>
2026-06-09 10:00:47 +0545

    add protectedroute for dashboard or update security

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,17 +4,21 @@ import Login from './pages/Login'
 import Signup from './pages/Signup'
 import Dashboard from './pages/Dashboard'
 import Tools from './pages/Tools'
+import ProtectedRoute from './components/ProtectedRoute'
+import { ThemeProvider } from './context/ThemeContext'
 
 function App() {
   return (
+    <ThemeProvider>
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/tools/:docId" element={<Tools />} />
+        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+        <Route path="/tools/:docId" element={<ProtectedRoute><Tools/></ProtectedRoute>} />
       </Routes>
     </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
